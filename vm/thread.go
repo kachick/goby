2261ed577d6dfa8885b19b1c2d2265cdf8f0c633
st0012 <a22301613@yahoo.com.tw>
2017-10-22 00:44:03 +0800

    Add test for error stacktraces.

--- vm/thread.go
+++ vm/thread.go
@@ -6,7 +6,6 @@ import (
 	"github.com/goby-lang/goby/vm/errors"
 	"os"
 	"strings"
-	"bytes"
 )
 
 type thread struct {
@@ -106,17 +105,13 @@ func (t *thread) hasError() (hasError bool) {
 }
 
 func (t *thread) reportErrorAndStop(err *Error) {
-	var stackTraces bytes.Buffer
-
 	cf := t.callFrameStack.top()
 	cf.stopExecution()
 
 	for i := 0; i < t.cfp-1; i++ {
-		stackTraces.WriteString(t.callFrameStack.callFrames[i].FileName())
-		stackTraces.WriteString("\n")
+		msg := fmt.Sprintf("%s:%d", t.callFrameStack.callFrames[i].FileName(), t.callFrameStack.callFrames[i].SourceLine())
+		err.stackTraces = append(err.stackTraces, msg)
 	}
-
-	err.stackTraces = stackTraces.String()
 
 	if t.vm.mode == NormalMode {
 		if t.isMainThread() {
